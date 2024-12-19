d240adb74b5b29c299dddde6a2366dfdfaa7c652
Azizos002 <azizdhifaoui@isima.u-monastir.tn>
2024-12-20 00:56:16 +0100

    home pages

---
+++ feu/src/Pages/Equipe.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import NavbarHome from '../Components/Navbar/NavbarHome';
+import Footer from '../Components/FooterHome/Footer';
+
+const Equipe = () => {
+    return (
+        <>
+            <NavbarHome />
+            <div className='pagesContainer'>
+                Equipe
+            </div>
+            <Footer />
+        </>
+    )
+}
+
+export default Equipe
---
+++ feu/src/Pages/Technologies.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import NavbarHome from '../Components/Navbar/NavbarHome';
+
+import './style.css'
+import Footer from '../Components/FooterHome/Footer';
+
+const Technologies = () => {
+    return (
+        <>
+            <NavbarHome />
+            <div className="pagesContainer">
+                Hi
+            </div>
+            <Footer />
+        </>
+    )
+}
+
+export default Technologies
